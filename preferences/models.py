53da4e888ffd0c852dac424cc287787cd5cf65f3
Matteo Hoch <minime@hochweb.com>
2014-03-03 20:31:26 -0600

    Preferences update

--- preferences/models.py
+++ preferences/models.py
@@ -7,14 +7,14 @@ class Preferences(models.Model):
 	hobby1 = models.CharField(max_length=20, blank=True, default='') #Generic choice handling?
 	hobby2 = models.CharField(help_text="optional", max_length=20, blank=True, default='')
 	hobby3 = models.CharField(help_text="optional", max_length=20, blank=True, default='')
-	alcohol = models.CharField(verbose_name="Favorite Alcohol", max_length=3, blank=True, choices=(('G', 'Gin'), ('W', 'Whiskey'), ('R', 'Rum'), ('V', 'Vodka'), ('T', 'Tequila'),('N', "Don't Drink"),))
+	alcohol = models.CharField(verbose_name="Favorite Alcohol", max_length=3, blank=True, help_text="We might send you some non-alcoholic beverages or creative products that pair well with a certain alcohol", choices=(('G', 'Gin'), ('W', 'Whiskey'), ('R', 'Rum'), ('V', 'Vodka'), ('T', 'Tequila'),('N', "Don't Drink"),))
 	#hometown = models.CharField(max_length=30, blank=True )
 	adj1 = models.CharField(verbose_name='Adjective to describe you?' , max_length=20, blank=True, default='')
 	adj2 = models.CharField(help_text="optional", verbose_name='Second adjective to describe you?' , max_length=20, blank=True, default='')
 	adj3 = models.CharField(help_text="optional", verbose_name='Third adjective to describe you?' , max_length=20, blank=True, default='')
 	fraternity = models.BooleanField(verbose_name="Are you in a fraternity?", default=1, choices=((0, 'Yes'), (1, 'No')))
 	party = models.BooleanField(verbose_name="Do you like to party?", default=1 , choices=((0, 'Yes'), (1, 'No')))
-	novelty = models.BooleanField(verbose_name="Do you like novelties?", default=0, choices=((0, 'Yes'), (1, 'No')))
+	novelty = models.BooleanField(verbose_name="Do you like funny gifts?", default=0, choices=((0, 'Yes'), (1, 'No')))
 	clip_on = models.BooleanField(verbose_name="Does a clip-on bowtie upset you?", default=1, choices=((0, 'Yes'), (1, 'No')))
 
 	#allow user to suggest their own preference (json?)
