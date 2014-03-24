e2138c9c7a36070d487f0406fdc17e96a204404b
Matteo Hoch <minime@hochweb.com>
2014-03-24 16:17:34 -0500

    Fixed 1 0 for preferences

--- preferences/models.py
+++ preferences/models.py
@@ -12,9 +12,9 @@ class Preferences(models.Model):
 	adj1 = models.CharField(verbose_name='Adjective to describe you?' , max_length=20, blank=True, default='')
 	adj2 = models.CharField(help_text="optional", verbose_name='Second adjective to describe you?' , max_length=20, blank=True, default='')
 	adj3 = models.CharField(help_text="optional", verbose_name='Third adjective to describe you?' , max_length=20, blank=True, default='')
-	fraternity = models.BooleanField(verbose_name="Are you in a fraternity?", default=1, choices=((0, 'Yes'), (1, 'No')))
-	party = models.BooleanField(verbose_name="Do you like to party?", default=1 , choices=((0, 'Yes'), (1, 'No')))
-	novelty = models.BooleanField(verbose_name="Do you like funny gifts?", default=0, choices=((0, 'Yes'), (1, 'No')))
-	clip_on = models.BooleanField(verbose_name="Does a clip-on bowtie upset you?", default=1, choices=((0, 'Yes'), (1, 'No')))
+	fraternity = models.BooleanField(verbose_name="Are you in a fraternity?", default=1, choices=((1, 'Yes'), (0, 'No')))
+	party = models.BooleanField(verbose_name="Do you like to party?", default=1 , choices=((1, 'Yes'), (0, 'No')))
+	novelty = models.BooleanField(verbose_name="Do you like funny gifts?", default=0, choices=((1, 'Yes'), (0, 'No')))
+	clip_on = models.BooleanField(verbose_name="Does a clip-on bowtie upset you?", default=1, choices=((1, 'Yes'), (0, 'No')))
 
 	#allow user to suggest their own preference (json?)
