42cddb1666dc063c3e42a5589f141d5c3888281a
Matteo Hoch <minime@hochweb.com>
2014-04-06 20:14:18 -0500

    initial

--- box/custom_settings.py
+++ box/custom_settings.py
@@ -64,21 +64,3 @@ EMAIL_HOST_PASSWORD = MANDRILL_API_KEY
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'no-reply@thenicepackage.com'
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['boiling-eyrie-3396', '.boiling-eyrie-3396']
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
