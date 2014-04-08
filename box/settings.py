e5e69b1435336831ba571efce24e72023fa8ee5f
Matteo Hoch <minime@hochweb.com>
2014-04-07 19:22:37 -0500

    Added sentry

--- box/settings.py
+++ box/settings.py
@@ -172,7 +172,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = '/app'
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
