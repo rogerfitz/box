fa05912a788f068d95cf487ba442eb43febffa49
Matteo Hoch <user@user-VirtualBox.(none)>
2014-04-13 20:42:12 -0500

    Fixed stupid auth_user

--- users/forms.py
+++ users/forms.py
@@ -13,6 +13,16 @@ class UserForm(UserCreationForm):
 		super(UserForm, self).__init__(*args, **kwargs)
 		self.fields['username'].label = 'Email'
 		self.fields['username'].help_text = 'Max of 30 characters'
+	def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+		username = self.cleaned_data["username"]
+		try:
+		    User._default_manager.get(username=username)
+		except User.DoesNotExist:
+		    return username
+		raise forms.ValidationError(self.error_messages['duplicate_username'])
+
 
 	class Meta:
 		model = User
